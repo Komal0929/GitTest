edcc12d0c76b91387ccc039132d12bd1e3756455
komal0929 <komalghare32@gmail.com>
2022-01-22 19:26:14 +0530

    java application file

---
+++ Test/src/main/java/study/TestApplication.java
@@ -0,0 +1,18 @@
+package study;
+
+
+public class TestApplication 
+{
+
+	public static void main(String[] args)
+	{
+		System.out.println(checkIfInputIsAnEvenNumber(122));
+		// Testing in the main method
+	}
+
+	public static boolean checkIfInputIsAnEvenNumber(int number)
+	{
+		return number % 2==0;
+	}
+
+}
